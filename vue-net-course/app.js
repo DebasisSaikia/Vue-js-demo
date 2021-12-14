e0b8f56e9e5de1c369a5d18f45916ffcf57a1ebb
Debasis Saikia <debasishsaikia136@gmail.com>
2021-12-14 15:19:52 +0530

    mouse event directive

--- vue-net-course/app.js
+++ vue-net-course/app.js
@@ -1,10 +1,27 @@
-const app=Vue.createApp({
-    data(){
-        return {
-            title:'Hello Vue Data',
-            age:24
-        }
+const app = Vue.createApp({
+  data() {
+    return {
+      title: "Hello Vue Data",
+      age: 24,
+      gender: "MALE",
+      show: true,
+      x:0,
+      y:0,
+    };
+  },
+  methods: {
+    changeTitle() {
+      this.title = "hey deba";
+    },
+    handleEvents() {
+      alert("Events ente");
+      this.title = "hey mouseOver"
+    },
+    mouseMove(e){
+        this.x=e.offsetX
+        this.y=e.offsetY
     }
-})
+  },
+});
 
-app.mount('#app');
+app.mount("#app");
